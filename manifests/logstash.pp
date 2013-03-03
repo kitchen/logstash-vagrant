da20eda0510fdaa2569bb2bae117fe27368774e8
Jeremy Kitchen <kitchen@kitchen.io>
2013-03-03 15:43:31 -0800

    committing for Electrical

---
+++ manifests/logstash.pp
@@ -0,0 +1,20 @@
+# elasticsearch needs to be up and running before logstash. oof
+
+package {"apache2":
+    ensure => present,
+}
+
+class {"elasticsearch":
+    pkg_source   => "/vagrant/elasticsearch-0.20.5.deb",
+    java_install => true,
+    config       => {
+        'node'   => { name => 'logstash-demo' },
+    },
+    status => 'disabled',
+}
+
+class {"logstash":
+    provider    => 'custom',
+    jarfile     => '/vagrant/logstash-1.1.9-monolithic.jar',
+    installpath => '/usr/share/java/logstash-1.1.9-monolithic.jar',
+}
